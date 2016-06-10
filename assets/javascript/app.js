ca3f6c5c4f66d923aa9cb20651edb79914f0afe4
Roy Hinojosa <roy.hinojosa@gmail.com>
2016-06-09 21:17:07 -0600

    changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+var beer = "";
+
+$('#beerSubmit').on('click', function() {
+	beer = $('#beerInput').val().trim();
+	console.log(beer);
+	$('#title').hide();
+	$('.form-inline').css('margin-top', '60px');
+	$('#beerInput').val("");
+	displayBeerStats();
+	return false;
+});
+
+function displayBeerStats() {
+	var key = "fe7efdc02eeb3b9ec8d79665a793e270";
+	var differentKey = "c02a0cf5f068d13a06050a1451d814f8";
+	var queryURL = "http://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&withBreweries=Y&format=json&key=" + key;
+	console.log(queryURL);
+	$.ajax({
+		url: queryURL,
+		type: 'GET',
+		async: false,
+		contentType: "application/json",
+		dataType: 'jsonp'
+	}).done(function(response) {
+		console.log(response);
+		var beerData = response.data;
+		console.log(beerData);
+	});
+}
