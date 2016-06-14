0ad0337aab3baa008aa6d95b54e27f897f663918
Bill <wkernan@gmail.com>
2016-06-13 22:56:53 -0500

    fixed gif and removed unused files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,8 +13,6 @@ var bars = [
 	}
 ]
 
-console.log(bars[0].beers[0]);
-
 $('#beerSubmit').on('click', function() {
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -23,18 +21,16 @@ $('#beerSubmit').on('click', function() {
 				lng: position.coords.longitude
 			};
 			initMap();
-			console.log(pos);
 		});
 	}
 	beer = $('#beerInput').val().trim();
-	console.log(beer);
 	$('#info').addClass('hide');
 	$('.form-inline').css('margin-top', '35px');
 	$('#beerInput').val("");
 	$('#newRow').empty();
 	$('#newRow').append("<tr><th>Name</th><th>Label</th><th>ABV</th><th>Description</th><th>Style</th><th>Brewery</th><th>Location</th></tr>");
 	displayBeerStats();
-	$("#loading-gif").show().css('display', 'block !important');
+	$("#loading-gif").removeClass('hide').css('display', 'block !important');
 	return false;
 });
 
@@ -43,15 +39,12 @@ function displayBeerStats() {
 	var key = "88FE890DEF0863F2929FFBC8575FF7F224A431E3";
 	var secretKey = "931C49726BD3C379BCA98A13BE0E0118E44BB308";
 	var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beer + "&limit=5&client_id=" + key + "&client_secret=" + secretKey;
-	console.log(queryURL);
 	$.ajax({
 		url: queryURL,
 		type: 'GET',
 	}).done(function(data) {
-		console.log(data);
 		$( "#loading-gif" ).hide();
 		var beerData = data.response.beers.items;
-		console.log(beerData);
 		beerData.forEach(function(b) {
 			$('#newRow').append("<tr><td>" + b.beer.beer_name + "</td><td><img src='" + b.beer.beer_label +"'></td><td>" + b.beer.beer_abv + "%</td><td>" + b.beer.beer_description + "</td><td>" + b.beer.beer_style + "</td><td><a href='" + b.brewery.contact.url + "' target='_blank'>" + b.brewery.brewery_name + "</a></td><td>" + b.brewery.location.brewery_city + ", " + b.brewery.location.brewery_state + " distance: " + distance + " mi</td></tr>");
 			$('#newRow').removeClass('hide');
@@ -91,15 +84,12 @@ function initMap() {
     destination: spot,
     travelMode: google.maps.TravelMode.DRIVING
   };
-  console.log(request.origin);
-  console.log(request.destination);
   directionsService.route(request, function(result, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(result);
     }
   });
   directionsDisplay.setMap(map);
-  //getDistance();
   var geocoder = new google.maps.Geocoder;
 
   var service = new google.maps.DistanceMatrixService;
@@ -116,24 +106,9 @@ function initMap() {
     } else {
       var originList = response.originAddresses;
       var destinationList = response.destinationAddresses;
-      console.log(response);
       $('#output').html(originList[0] + " to <a href='" + bars[0].url + "' target='_blank'>" + bars[0].name + "</a>: " + response.rows[0].elements[0].distance.text + " in " + response.rows[0].elements[0].duration.text);
-      console.log('distance works');
     }
   });
 }
-
-/* Was Getting CORS Error Again
-function getDistance() {
-	var distKey = "AIzaSyDdCWO-KjO5Gp_jN19FuqyPyjr84sbgtO0";
-	var distUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + pos.lat + "," + pos.lng + "&destinations=" + spot.lat + "," + spot.lng + "&key=" + distKey;
-	//https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyDdCWO-KjO5Gp_jN19FuqyPyjr84sbgtO0
-	$.ajax({
-		url: distUrl,
-		type: 'GET',
-	}).done(function(data) {
-		console.log(data);
-	});
-}*/
 
 
